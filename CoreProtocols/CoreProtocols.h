48b6535d50d49547d29b16d6ebf621cb0713cd7f
Daher Alfawares <daher.alfawares@live.com>
2016-10-03 16:40:50 -0500

    adding

---
+++ CoreProtocols/CoreProtocols.h
@@ -0,0 +1,19 @@
+//
+//  CoreProtocols.h
+//  CoreProtocols
+//
+//  Created by Daher Alfawares on 10/3/16.
+//  Copyright © 2016 Solstice Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreProtocols.
+FOUNDATION_EXPORT double CoreProtocolsVersionNumber;
+
+//! Project version string for CoreProtocols.
+FOUNDATION_EXPORT const unsigned char CoreProtocolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreProtocols/PublicHeader.h>
+
+
